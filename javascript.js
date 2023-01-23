984baf50db526cdf3ef22b7a8a8f2ab49bc15f54
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-23 23:04:04 +0700

    Fix bug in confirmPassword function to remove old class

--- javascript.js
+++ javascript.js
@@ -5,11 +5,13 @@ const pwd_errors=document.querySelectorAll('#pwd_error');
 function confirmPassword() {
     if (user_pwd.value == user_confirm_pwd.value) {
         for (let pwd_error of pwd_errors){
-            pwd_error.classList.add("clear")
+            pwd_error.classList.add("clear");
+            pwd_error.classList.remove("error");
         }
     } else {
         for (let pwd_error of pwd_errors){
-            pwd_error.classList.add("error")
+            pwd_error.classList.add("error");
+            pwd_error.classList.remove("clear");
         }
     }
     console.log(pwd_error)
