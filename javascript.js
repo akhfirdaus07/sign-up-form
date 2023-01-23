0cfa4b1d63dfdaa92c0750e149a9a1c88c22bb94
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-23 23:00:02 +0700

    Add password and confirm password matching on javascript

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const user_pwd=document.getElementById('user_pwd');
+const user_confirm_pwd=document.getElementById('user_confirm_pwd');
+const pwd_errors=document.querySelectorAll('#pwd_error');
+
+function confirmPassword() {
+    if (user_pwd.value == user_confirm_pwd.value) {
+        for (let pwd_error of pwd_errors){
+            pwd_error.classList.add("clear")
+        }
+    } else {
+        for (let pwd_error of pwd_errors){
+            pwd_error.classList.add("error")
+        }
+    }
+    console.log(pwd_error)
+}
+
+document.getElementById("user_confirm_pwd").addEventListener("change", confirmPassword);
